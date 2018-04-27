75bbfa8fdaa9657069a16508072a4dbd8912e0f4
Klauzevitc <klauzevitc@gmail.com>
2018-04-27 12:11:10 +0300

    add custom header

--- src/Providers/QuadriosServiceProvider.php
+++ src/Providers/QuadriosServiceProvider.php
@@ -2,7 +2,10 @@
  
 namespace Quadrios\Providers;
  
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
  
 class QuadriosServiceProvider extends ServiceProvider
 {
@@ -14,4 +17,20 @@ class QuadriosServiceProvider extends ServiceProvider
 	{
  
 	}
+	
+	
+	
+	/**
+	 * Boot a template for the header that will be displayed in the template plugin instead of the original header.
+	 */
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+        {
+           $partial->set('header', 'Quadrios::content.QuadriosHeader');
+        }, 0);
+        return false;
+    }
+	
+	
 }
