cf993f690ff1e91f3daa3ed9550e957ce932cf3b
Klauzevitc <klauzevitc@gmail.com>
2018-05-03 09:27:07 +0300

    111

--- src/Providers/QuadriosServiceProvider.php
+++ src/Providers/QuadriosServiceProvider.php
@@ -32,20 +32,21 @@ class QuadriosServiceProvider extends ServiceProvider{
 		   $partial->set('footer', 'Quadrios::PageDesign.Partials.QuadriosFooter');
 		   //$partial->set('page-design', 'Quadrios::PageDesign.QuadriosPageDesign');
         }, 0);
-		
-		
-		$eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container){     
-			
+        return false;
+    }
+	
+	
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)    {
+			$eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container){
 			// The style is imported in the <head> on the PageDesign.twig of Ceres
             $container->addStyleTemplate('Quadrios::content.QuadriosCSS');
 			
 			// The script is imported in the Footer.twig of Ceres
             $container->addScriptTemplate('Quadrios::content.QuadriosScript');
         }, self::PRIORITY);
-		
-		
-        return false;
+		 return false;
     }
+	
 	
 	
 	
